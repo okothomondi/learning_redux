73e0f5762860c2990c88b2fd15498f72451a80a5
Okoth Omondi <okoth.omondi@interswitchgroup.com>
2020-02-07 13:12:24 +0300

    end of class

---
+++ src/components/PostForm.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+import {createPost} from "../actions/postAtcion";
+
+class PostForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            body: ''
+        }
+    }
+
+    onChange = e => this.setState({[e.target.name]: e.target.value});
+
+    onSubmit = e => {
+        e.preventDefault();
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        };
+        this.props.createPost(post);
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Add post</h1>
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        <label> Title: </label><br/>
+                        <input type='text' name='title' value={this.state.title} onChange={this.onChange}/>
+                    </div>
+                    <div>
+                        <label> Body: </label><br/>
+                        <textarea name='body' value={this.state.body} onChange={this.onChange}/>
+                    </div>
+                    <br/>
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+PostForm.prototypes = {
+    createPost: PropTypes.func.isRequired
+};
+
+export default connect(null, {createPost})(PostForm);
---
+++ src/components/Posts.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+import {fetchPosts} from "../actions/postAtcion";
+
+class Posts extends Component {
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        if (nextProps.newPost) this.props.posts.unshift(nextProps.newPost);
+    }
+
+    render() {
+        const postItems = this.props.posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+        ));
+        return (
+            <div>
+                <h1>posts</h1>
+                {postItems}
+            </div>
+        );
+    }
+}
+
+Posts.protoTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired,
+    newPost: PropTypes.object
+};
+
+const mapStateToProps = state => ({
+    posts: state.posts.items,
+    newPost: state.posts.item
+});
+
+export default connect(mapStateToProps, {fetchPosts})(Posts);
